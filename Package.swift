ef7950e39a7190a7811ba580e3d84e172fe6243e
Jeffrey Macko <github.jm@macko.fr>
2021-09-05 02:21:39 +0200

    minor

--- Package.swift
+++ Package.swift
@@ -4,22 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "FileSorter",
-    products: [
-      .executable(name: "sorter", targets: ["FileSorter"])
-    ],
-    dependencies: [
-      .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.5.0")),
-      .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "FileSorter",
-          dependencies: [
-            .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            .product(name: "Logging", package: "swift-log"),
-          ]),
-    ]
+  name: "FileSorter",
+  products: [
+    .executable(name: "sorter", targets: ["FileSorter"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.5.0")),
+    .package(url: "https://github.com/apple/swift-log", from: "1.4.2"),
+  ],
+  targets: [
+    .target(
+      name: "FileSorter",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Logging", package: "swift-log"),
+      ]),
+  ]
 )
--- Sources/FileSorter/Helper.swift
+++ Sources/FileSorter/Helper.swift
@@ -4,8 +4,8 @@ extension String : Error {}
 
 extension Sequence {
   func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>, order: @escaping (T, T) -> Bool) -> [Element] {
-        return sorted { a, b in
-          return order(a[keyPath: keyPath], b[keyPath: keyPath])
-        }
+    return sorted { a, b in
+      return order(a[keyPath: keyPath], b[keyPath: keyPath])
     }
+  }
 }
